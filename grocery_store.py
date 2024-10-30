74bb76e2b556db2009a04aefb59dece3d331cbb8
TheKingsident <kingsident@gmail.com>
2024-10-30 01:41:46 +0100

    Refactor grocery_store.py to include enhanced user authentication

--- grocery_store.py
+++ grocery_store.py
@@ -19,7 +19,13 @@ def authenticate_user(users):
     """
     username = input("Username: ")
     password = input("Password: ")
-    return users.get(username) == password
+
+    user = next((user for user in users if user['username'] == username), None)
+
+    if user and user['password'] == password:
+        return True, username, user['type']
+    
+    return False, None, None
 
 def main(grocery_file, transaction_file, user_file):
     grocery_data = load_grocery_data(grocery_file)
@@ -29,9 +35,16 @@ def main(grocery_file, transaction_file, user_file):
     # print(json.dumps(transaction_data, indent=4))
     # print(json.dumps(grocery_data, indent=4))
     
-    if not authenticate_user(user_data):
+    authemticated, username, user_type = authenticate_user(user_data)
+
+    if not authemticated:
         print("Authentication failed")
         return
+    
+    print(f"Welcome {username}!, you are logged in as a {user_type}.")
+
+    while True:
+        if 
 
 
 if __name__ == "__main__":
